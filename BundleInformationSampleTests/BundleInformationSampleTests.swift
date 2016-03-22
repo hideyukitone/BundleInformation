2b0023a9d1220b82089c7e9c9eda29ad4ab59d68
hideyuki <hideyukitone@gmail.com>
2016-03-23 02:21:16 +0900

    Initial Commit

---
+++ BundleInformationSampleTests/BundleInformationSampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  BundleInformationSampleTests.swift
+//  BundleInformationSampleTests
+//
+//  Created by 大國 嗣元 on 2016/03/23.
+//  Copyright © 2016年 hideyuki. All rights reserved.
+//
+
+import XCTest
+@testable import BundleInformationSample
+
+class BundleInformationSampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
